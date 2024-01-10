6b56ecc3b696aca450e3e167780b14cfda86f6c3
buljad <i.zhuravskii@g.nsu.ru>
2024-01-11 02:49:17 +0700

    Add empty Login component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,10 +8,10 @@ const Header = () => {
         <div className={classes.header__content}>
             <h2 className={classes.header__content__logo}>timetable</h2>
             <nav className={classes.header__content__nav}>
-                <div className={classes.header__content__nav__label}>it's just a timetable webpage</div>
+                <div>it's just a timetable webpage</div>
             </nav>
             <div className={classes.header__content__toggle}>
-                <BiMenuAltRight/>
+                <BiMenuAltRight />
             </div>
         </div>
     </header>
