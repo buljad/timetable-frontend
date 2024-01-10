68da4379f7ef0d8c04875e224e18e7cd9dbcdd7c
buljad <i.zhuravskii@g.nsu.ru>
2024-01-11 01:51:05 +0700

    Now it is adaptive for resizing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import classes from './Header.module.scss'
-//import { BiMenuAltRight } from 'react-icons/bi'
+import { BiMenuAltRight } from 'react-icons/bi'
 //import { AiOutlineClose } from 'react-icons/ai'
 
 const Header = () => {
     return <header className={classes.header}>
         <div className={classes.header__content}>
             <h2 className={classes.header__content__logo}>timetable</h2>
-            <div>it's just a timetable webpage</div>
             <nav className={classes.header__content__nav}>
-                
+                <div className={classes.header__content__nav__label}>it's just a timetable webpage</div>
             </nav>
+            <div className={classes.header__content__toggle}>
+                <BiMenuAltRight/>
+            </div>
         </div>
     </header>
 }
